75a4ec19646de469a104737771dd4273bd049952
CheyBonnie <116781969+CheyBonnie@users.noreply.github.com>
2022-10-26 22:43:55 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+// Kelvin Project
+
+// The vaule saved to kelvin will remain constant
+const kelvin = 283;
+// var kelvin =293
+
+// The difference of celsius and kelvin is 273 
+var celsius = kelvin - 273;
+
+// This is the equation for celsius to farenheit
+var fahrenheit = celsius * (9/5) + 32;
+
+//fahrentheit produces decimals, floor will round down. 
+fehrenheit = Math.floor(fahrenheit);
+
+var newton = celsius * (33/100);
+
+newton = Math.floor(newton);
+
+console.log(`The temerature is ${fahrenheit} degrees Fahrenheit.`);
+
+console.log(`The temerature is ${newton} degrees Fahrenheit.`);
